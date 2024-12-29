3d2cfeae3eb99d08f98d4fe659fb6e3d1df82558
ZachLTech <zlopezzl2021@gmail.com>
2024-12-29 14:08:28 -0500

    lets goo you can upload files now and it'll make a room from that imported excalidraw file

--- app/index.js
+++ app/index.js
@@ -1,5 +1,5 @@
 import puppeteer from "puppeteer";
-
+import { readFile } from 'fs/promises';
 
 async function createRoom() {
     const browser = await puppeteer.launch({
@@ -64,7 +64,75 @@ async function createRoom() {
 }
 
 async function createRoomFromFile(input) {
-    
+    const browser = await puppeteer.launch({
+        headless: false,
+        args: ['--window-size=817,531']
+    });
+    const page = await browser.newPage();
+    const timeout = 5000;
+    page.setDefaultTimeout(timeout);
+
+    await page.setViewport({
+        width: 817,
+        height: 531
+    });
+
+    await page.goto('https://excalidraw.com');
+
+    const context = browser.defaultBrowserContext();
+    await context.overridePermissions('https://excalidraw.com', [
+        "clipboard-read",
+        "clipboard-write",
+        "clipboard-sanitized-write",
+    ]);
+
+    await new Promise(resolve => setTimeout(resolve, 2000));
+
+    await page.evaluate(async (input) => {
+        await navigator.clipboard.writeText(input);
+    }, input);
+
+    await page.keyboard.down('Control');
+    await page.keyboard.press('a');
+    await page.keyboard.press('v');
+    await page.keyboard.up('Control');
+
+    await new Promise(resolve => setTimeout(resolve, 500));
+
+    const button = await page.waitForSelector('div.layer-ui__wrapper__top-right > div svg');
+    await button.click({
+        offset: {
+            x: 4.87493896484375,
+            y: 18.01041603088379,
+        }
+    });
+
+    await Promise.race([
+        page.waitForSelector('::-p-aria(Start session)'),
+        page.waitForSelector('body > div.excalidraw div:nth-of-type(3) > button'),
+        page.waitForSelector('::-p-xpath(/html/body/div[2]/div/div[2]/div/div/div/div[3]/button)'),
+    ]).then(async (element) => {
+        await element.click({
+            offset: {
+                x: 128.32290649414062,
+                y: 9.315963745117188,
+            }
+        });
+    }).catch(error => {
+        console.error('Failed to find or click the button:', error);
+        throw error;
+    });
+
+    await new Promise(resolve => setTimeout(resolve, 1000));
+
+    const roomUrl = await page.evaluate(() => {
+        const input = document.querySelector('input[value^="https://excalidraw.com/#room="]');
+        return input.value;
+    });
+
+    await browser.close();
+
+    return roomUrl
 }
 
 async function backupRoom(roomUrl) {
@@ -158,5 +226,9 @@ async function backupRoom(roomUrl) {
 // const room = await createRoom();
 // const backup = await backupRoom('https://excalidraw.com/#room=e184e813cd5a12b3a8df,C8vJYXT69yl-OfL5OU9Iaw');
 
+// await createRoomFromFile('input.excalidraw')
 
-await createRoomFromFile('input.excalidraw')
+const fileContent = await readFile('./input.excalidraw', 'utf8')
+const roomFromFile = await createRoomFromFile(fileContent)
+
+console.log(roomFromFile)
