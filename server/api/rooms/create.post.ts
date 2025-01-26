23d2acaa5832d14e3e7941dd91da4aadb7725704
ZachLTech <zlopezzl2021@gmail.com>
2025-01-26 01:32:42 -0500

    MVP API done I think

--- server/api/rooms/create.post.ts
+++ server/api/rooms/create.post.ts
@@ -1,5 +1,5 @@
 import { getServerSession } from "#auth";
-import { Room } from "@prisma/client";
+import puppeteer from "puppeteer";
 
 /* 
     Body Structure:
@@ -9,16 +9,122 @@ import { Room } from "@prisma/client";
         thumbnail: '',
         roomType: '',
         status: '',
-        excalidrawUrl: '',
-        readOnlyUrl: ''
+        readOnlyUrl: '',
+        excalidrawFile: ''
     }
 */
 
 // If readonlyurl, create with that, if excalidrawurl, create with that.
 
-type newRoomInput = Partial<
-    Pick<Room, "userId" | "name" | "description" | "thumbnail" | "roomType" | "status" | "excalidrawUrl" | "readOnlyUrl">
->;
+function generateRandomBase64(length: number) {
+    const array = new Uint8Array(length);
+    crypto.getRandomValues(array);
+    return btoa(String.fromCharCode.apply(null, [...array]))
+        .replace(/\+/g, '-')
+        .replace(/\//g, '_')
+        .replace(/=/g, '');
+}
+
+async function createRoom(input?: any) { // Input can be #json URL or .excalidraw file. If empty it'll create new empty room
+    if (!input) {
+        const roomId = generateRandomBase64(20);
+        const encryptionKey = generateRandomBase64(24).slice(0, 22);
+        
+        return `https://excalidraw.com/#room=${roomId},${encryptionKey}`;
+    }
+    
+    const browser = await puppeteer.connect({ 
+        browserWSEndpoint: 'wss://browserless.zachl.tech',
+    });
+    const page = await browser.newPage();
+    const timeout = 5000;
+    page.setDefaultTimeout(timeout);
+
+    await page.setViewport({
+        width: 817,
+        height: 531
+    });
+
+    if (input.includes('excalidraw.com/#json=')) {
+        await page.goto(input);
+        await new Promise(resolve => setTimeout(resolve, 2000));
+    } else {
+        await page.goto('https://excalidraw.com');
+
+        const context = browser.defaultBrowserContext();
+        await context.overridePermissions('https://excalidraw.com', [
+            "clipboard-read",
+            "clipboard-write",
+            "clipboard-sanitized-write",
+        ]);
+
+        await new Promise(resolve => setTimeout(resolve, 2000));
+
+        await page.evaluate(async (input) => {
+            // @ts-ignore
+            await navigator.clipboard.writeText(input);
+        }, input);
+
+        await page.keyboard.down('Control');
+        await page.keyboard.press('a');
+        await page.keyboard.press('v');
+        await page.keyboard.up('Control');
+
+        await new Promise(resolve => setTimeout(resolve, 500));
+    }
+
+    const button = await page.waitForSelector('div.layer-ui__wrapper__top-right > div svg');
+    if (!button) {
+        throw createError({
+            statusCode: 501,
+            statusMessage:
+                "There was an issue creating your room.",
+        });
+    }
+    await button.click({
+        offset: {
+            x: 4.87493896484375,
+            y: 18.01041603088379,
+        }
+    });
+
+    await Promise.race([
+        page.waitForSelector('::-p-aria(Start session)'),
+        page.waitForSelector('body > div.excalidraw div:nth-of-type(3) > button'),
+        page.waitForSelector('::-p-xpath(/html/body/div[2]/div/div[2]/div/div/div/div[3]/button)'),
+    ]).then(async (element) => {
+        if (!element) {
+            throw createError({
+                statusCode: 501,
+                statusMessage:
+                    "There was an issue creating your room.",
+            });
+        }
+        await element.click({
+            offset: {
+                x: 128.32290649414062,
+                y: 9.315963745117188,
+            }
+        });
+    }).catch(error => {
+        console.error('Failed to find or click the button:', error);
+        throw error;
+    });
+
+    await new Promise(resolve => setTimeout(resolve, 1000));
+
+    const roomUrl = await page.evaluate(() => {
+        // @ts-ignore
+        const input = document.querySelector('input[value^="https://excalidraw.com/#room="]');
+        return input.value;
+    });
+
+    await new Promise(resolve => setTimeout(resolve, 1000));
+
+    await browser.close();
+
+    return roomUrl
+}
 
 export default eventHandler(async (event) => {
     const body = await readBody(event);
@@ -52,7 +158,7 @@ export default eventHandler(async (event) => {
         });
     }
 
-    const newData: newRoomInput = {
+    const newData: any = {
         userId: user.id
     };
 
@@ -95,6 +201,14 @@ export default eventHandler(async (event) => {
 
     if (body.readOnlyUrl) {
         newData.readOnlyUrl = body.readOnlyUrl;
+    }
+
+    if (body.excalidrawFile) {
+        newData.excalidrawUrl = await createRoom(body.excalidrawFile)
+    } else if (body.readOnlyUrl) {
+        newData.excalidrawUrl = await createRoom(body.readOnlyUrl)
+    } else {
+        newData.excalidrawUrl = await createRoom()
     }
     
     const newRoom = await event.context.prisma.room.create({
